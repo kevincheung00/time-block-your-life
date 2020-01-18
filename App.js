eb00069551cca52523eb793183e35f1e65a49d15
kevincheung00 <kevin.cheung.sh@gmail.com>
2020-01-17 21:25:55 -0500

    Created local server and index

--- App.js
+++ App.js
@@ -1,8 +1,24 @@
-function greeting(greet){
+const http = require('http');
+const fs = require('fs');
 
-console.log(greet);
+const hostname = '127.0.0.1';
+const port = 3000;
 
-}
+fs.readFile('index.html', (err, html) => {
+  if (err){
+    throw err;
+  }
+
+  const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-type', 'text/html');
+    res.write(html);
+    res.end();
+  });
+
+  server.listen(port, hostname, () => {
+    console.log('Server started on port ' + port);
+  });
 
 
-greeting("Hello World");
+});
